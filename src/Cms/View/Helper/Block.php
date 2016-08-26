40748f90898d2d05c6cba027c6851acbb0ba8e81
shinesoftware <info@shinesoftware.com>
2016-08-26 15:14:18 +0200

    Readme updated

--- src/Cms/View/Helper/Block.php
+++ src/Cms/View/Helper/Block.php
@@ -1,68 +1,68 @@
-<?php 
+<?php
 namespace Cms\View\Helper;
+
+use Cms\Service\BlockService;
 use Zend\View\Helper\AbstractHelper;
 use Zend\ServiceManager\ServiceLocatorAwareInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
-class Block extends AbstractHelper implements ServiceLocatorAwareInterface {
-	
-	protected $serviceLocator;
-	 
-	/**
-	 * Set the service locator.
-	 *
-	 * @param $serviceLocator ServiceLocatorInterface       	
-	 * @return CustomHelper
-	 */
-	public function setServiceLocator(ServiceLocatorInterface $serviceLocator) {
-		$this->serviceLocator = $serviceLocator;
-		return $this;
-	}
-	/**
-	 * Get the service locator.
-	 *
-	 * @return \Zend\ServiceManager\ServiceLocatorInterface
-	 */
-	public function getServiceLocator() {
-		return $this->serviceLocator;
-	}
+class Block extends AbstractHelper
+{
 
-    public function __invoke($block, $showAlertbox=true)
+    /**
+     * @var LanguageService
+     */
+    protected $service;
+
+    /**
+     * @var MvcTranslator
+     */
+    protected $translator;
+
+    /**
+     * LanguageCodes constructor.
+     * @param LanguageService $service
+     * @param \Zend\Mvc\I18n\Translator $translator
+     */
+    public function __construct(BlockService $service, \Zend\Mvc\I18n\Translator $translator)
     {
-    	$strBlock = null;
-    	
-    	if(!empty($block)){
-			$serviceLocator = $this->getServiceLocator()->getServiceLocator();
-	    	$theblock = $serviceLocator->get('BlockService');
-			$translator = $serviceLocator->get('translator');
-			$locale = $translator->getLocale();
-			$fallback = $translator->getFallbackLocale();
-
-			// check and get the locale version if it is not exists a fallback version will be print
-			$theBlock = $theblock->findByPlaceholder($block, $locale);
-			if(!empty($theBlock)){
-				$strBlock  = $theBlock->getContent();
-			}else{
-			    // Check if the fallback locale version is present
-			    $theBlock = $theblock->findByPlaceholder($block, $fallback);
-			    if(!empty($theBlock)){
-			        $strBlock  = $theBlock->getContent();
-			    }else{
-
-					try {
-						return $this->view->$block();
-					} catch (\Exception $e) {
-
-					}
-    			    if($showAlertbox){
-    				    $strBlock = "<div class=\"alert alert-danger\">" . sprintf($translator->translate("Block %s%s%s doesn't found!"), "<strong>", $block, " ($locale)</strong>") . "</div>";
-    			    }else{
-    			        $strBlock = sprintf($translator->translate("Block %s%s%s doesn't found!"), "", $block, "");
-    			    }
-			    }
-			}
-    	}
-    	return $strBlock;
+        $this->service = $service;
+        $this->translator = $translator;
     }
-    
+
+    public function __invoke($block, $showAlertbox = true)
+    {
+        $strBlock = null;
+
+        if (!empty($block)) {
+            $locale = $this->translator->getLocale();
+            $fallback = $this->translator->getFallbackLocale();
+
+            // check and get the locale version if it is not exists a fallback version will be print
+            $theBlock = $this->service->findByPlaceholder($block, $locale);
+            if (!empty($theBlock)) {
+                $strBlock = $theBlock->getContent();
+            } else {
+                // Check if the fallback locale version is present
+                $theBlock = $this->service->findByPlaceholder($block, $fallback);
+                if (!empty($theBlock)) {
+                    $strBlock = $theBlock->getContent();
+                } else {
+
+                    try {
+                        return $this->view->$block();
+                    } catch (\Exception $e) {
+
+                    }
+                    if ($showAlertbox) {
+                        $strBlock = "<div class=\"alert alert-danger\">" . sprintf($this->translator->translate("Block %s%s%s doesn't found!"), "<strong>", $block, " ($locale)</strong>") . "</div>";
+                    } else {
+                        $strBlock = sprintf($this->translator->translate("Block %s%s%s doesn't found!"), "", $block, "");
+                    }
+                }
+            }
+        }
+        return $strBlock;
+    }
+
 }
